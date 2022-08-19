cb56b45af199f90ae1a5c274ff7d20660fecb2ee
diegoteos <diegoteos@hotmail.com>
2022-08-18 22:15:33 -0600

    header

---
+++ app/vistas/inc/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="txt/css" href="<?php echo RUTA_URL?>/css/estilos.css">
+    <title></title>
+</head>
+<body>
+
+</body>
+</html>
